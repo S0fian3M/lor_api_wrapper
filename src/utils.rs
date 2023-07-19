77d28c01f7c9eb2fbebf682e1af11a456a06bdf1
sofianemazieres <100087250+S0fian3M@users.noreply.github.com>
2023-07-19 19:38:51 +0200

    Recode JSON read and write

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,24 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{Write, Read};
 use std::path::Path;
 
-pub fn read_json_file(file_path: &Path) -> Result<HashMap<String, serde_json::Value>, Box<dyn std::error::Error>> {
-    let mut file = File::open(file_path)?;
+/**
+* Read JSON file to HashMap.
+*/
+pub fn read_json_file(json_file: &Path) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    let mut file = File::open(json_file)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    Ok(HashMap<String, serde_json::Value> = serde_json::from_str(&contents)?)
+    let json_value: serde_json::Value = serde_json::from_str(&contents)?;
+    Ok(json_value)
 }
 
-pub fn write_json_file(data: &HashMap<String, serde_json::Value>, file_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = File::create(file_path)?;
-    let json_string = serde_json::to_string_pretty(data)?;
-    file.write_all(json_string.as_bytes())?;
+/**
+* Write HashMap to JSON file.
+*/
+fn write_json_file(json_file: &str, data: &serde_json::Value) -> Result<(), Box<dyn std::error::Error>> {
+    let serialized_data = serde_json::to_string(data)?;
+    let mut file = File::create(json_file)?;
+    file.write_all(serialized_data.as_bytes())?;
     Ok(())
 }
