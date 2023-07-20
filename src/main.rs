30d8616da260f6338aba8a555aa053b1fdf9272b
sofianemazieres <100087250+S0fian3M@users.noreply.github.com>
2023-07-20 07:45:19 +0200

    Add card set and tests

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,13 @@ use log::LevelFilter;
 use log4rs::append::console::ConsoleAppender;
 use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::pattern::PatternEncoder;
-use serde_json::Value;
+use std::path::Path;
 
 use lor_api_wrapper::{GameStatus, LoRClient};
+mod utils;
+mod card;
 
-fn main() {
-    // Configure logging
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "{d} {l} - {m}{n}",
-        )))
-        .build();
-
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .build(
-            Root::builder()
-                .appender("stdout")
-                .build(LevelFilter::Info),
-        )
-        .unwrap();
-
-    log4rs::init_config(config).unwrap();
-
-
+fn test_game_status() {
     let api_key = "your_api_key".to_string(); // TODO: .env file
     let port = 21337; // Port by default
     let client = LoRClient::new(api_key, port);
@@ -48,4 +31,31 @@ fn main() {
             eprintln!("Error: {:?}", err);
         }
     }
+}
+
+// TODO: download automatically the sets of card you want
+// + online REST API
+fn main() {
+    // Configure logging
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "{d} {l} - {m}{n}",
+        )))
+        .build();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(
+            Root::builder()
+                .appender("stdout")
+                .build(LevelFilter::Info),
+        )
+        .unwrap();
+
+    log4rs::init_config(config).unwrap();
+
+    let all_cards: serde_json::Value = utils::read_json_file(Path::new("./data/set7b-en_us.json")).unwrap();
+    let first_card: card::Card = card::Card::new(all_cards[0].clone());
+    println!("{:?}", first_card);
+    test_game_status();
 }
