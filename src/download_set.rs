f23cbc1bebeb1cf8c14f57a5e144a582503083a2
sofianemazieres <100087250+S0fian3M@users.noreply.github.com>
2023-07-22 08:53:04 +0200

    Functions to download card sets online

---
+++ src/download_set.rs
@@ -0,0 +1,30 @@
+use reqwest;
+use serde::{Deserialize, Serialize};
+
+fn get_card_set_online(set_number: i32, region: &str, lite: bool) -> Result<serde_json::Value, reqwest::Error> {
+    let url = if lite {
+        format!(
+            "http://dd.b.pvp.net/latest/set{}/{}/data/set{}-{}.zip",
+            set_number, region, set_number, region
+        )
+    } else {
+        format!(
+            "http://dd.b.pvp.net/latest/set{}/{}/data/set{}-lite-{}.zip",
+            set_number, region, set_number, region
+        )
+    }
+
+    let response = reqwest::blocking::get(&url)?;
+    Ok(response.json()?)
+}
+
+// Function to get lor globals
+fn get_globals_online(region: &str) -> Result<serde_json::Value, reqwest::Error> {
+    let url = format!(
+        "https://dd.b.pvp.net/latest/core/{}/data/globals-{}.zip",
+        region, region
+    );
+
+    let response = reqwest::blocking::get(&url)?; 
+    Ok(response.json()?)
+}
