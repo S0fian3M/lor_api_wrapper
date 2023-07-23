0e34e485981a496aa75f953c34c2da8bfb96dc80
sofianemazieres <100087250+S0fian3M@users.noreply.github.com>
2023-07-23 20:44:04 +0200

    Deck improvements

--- src/deck.rs
+++ src/deck.rs
@@ -1,48 +1,42 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::Read;
-use std::io::Write;
-use std::path::Path;
-use lor_api_wrapper::Card;
+use lordeckcodes::{CardCodeAndCount, encoder};
+
+use crate::card::Card;
 
 /**
-* A deck is a pile of cards, some ids and a win rate.
+* A deck is a list of cards, some ids and a win rate.
 */
-struct Deck {
-    cards: Vec<(Card, i32)>,
+pub struct Deck {
+    cards: HashMap<Card, i32>,
     deck_code: Option<String>,
-    deck_id: Option<i32>,
-    wins: i32,
-    losses: i32,
+    wins: i16,
+    losses: i16,
 }
 
 impl Deck {
-    fn new(deck_informations: &HashMap<String, serde_json::Value>) -> Self {
-        let cards_in_deck = deck_informations.get("CardsInDeck").and_then(|cards| cards.as_object()).unwrap_or(&HashMap::new());
-        let deck_code = deck_informations.get("DeckCode").and_then(|code| code.as_str()).map(|s| s.to_owned());
-        let deck_id = deck_informations.get("deck_id").and_then(|id| id.as_i64()).map(|i| i as i32);
-        let wins = deck_informations.get("wins").and_then(|w| w.as_i64()).map(|i| i as i32).unwrap_or(0);
-        let losses = deck_informations.get("losses").and_then(|l| l.as_i64()).map(|i| i as i32).unwrap_or(0);
-        
-        let mut cards = Vec::new();
-        
-        for (card, amount) in cards_in_deck.iter() {
-            if let (Some(card_code), Some(count)) = (card.as_str(), amount.as_i64()) {
-                cards.push((Card::new(card_code.to_owned(), count as i32), count as i32));
-            }
+    pub fn new(cards: Vec<Card>, wins: i16, losses: i16) -> Self {
+        let mut cards_in_deck = HashMap::new();
+        for card in cards {
+            let count = cards_in_deck.entry(card).or_insert(0);
+            *count += 1;
         }
+
+        let cards_as_lordeckcodes_format: Vec<CardCodeAndCount> = cards_in_deck.iter().map(|(card, count)| {
+            CardCodeAndCount::from_data(&card.card_code.clone().unwrap(), *count).unwrap()
+        }).collect();
+
+        let deck_code = Some(encoder::code_from_deck(&lordeckcodes::Deck::from_vec(cards_as_lordeckcodes_format)).unwrap());
         
         Deck {
-            cards,
+            cards: cards_in_deck,
             deck_code,
-            deck_id,
             wins,
             losses,
         }
     }
 }
 
-impl PartialEq for Card {
+impl PartialEq for Deck {
     fn eq(&self, other: &Self) -> bool {
         self.deck_code == other.deck_code
     }
